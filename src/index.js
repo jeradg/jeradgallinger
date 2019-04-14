e3d2de996ef22c8ec7cac9d022d5e957e7ce24a0
Jerad Gallinger <jerad@jeradgallinger.ca>
2019-04-14 19:47:49 -0400

    Change terminal cursor and styling

--- src/index.js
+++ src/index.js
@@ -1,14 +1,2 @@
 import './css/style.css';
 import './favicon.ico';
-
-const themeSwitcherEl = document.getElementById('theme-switcher-button');
-
-themeSwitcherEl.addEventListener('click', function() {
-  const bodyEl = document.getElementsByTagName('body')[0];
-
-  if (bodyEl.classList.contains('light-theme')) {
-    bodyEl.classList.remove('light-theme');
-  } else {
-    bodyEl.classList.add('light-theme');
-  }
-});
