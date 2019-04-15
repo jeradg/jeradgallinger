724e00abf3c5781212ef7d5e04a0cdc006c0dff3
Jerad Gallinger <jerad@jeradgallinger.ca>
2019-04-15 11:52:53 -0400

    Fix cursor styling Remove slight jankiness from loading animation Refactor and add comments to JS

--- src/index.js
+++ src/index.js
@@ -1,31 +1,43 @@
 import './css/style.css';
 import './favicon.ico';
 
-const loadingCommandEl = document.getElementById('terminal-loading-command');
-const loadingCommandText = loadingCommandEl.textContent;
-
-loadingCommandEl.textContent = '';
-
-const chars = loadingCommandText.split('');
-
-function onLoaded() {
-  return setTimeout(function() {
-    document.getElementById('terminal-loading-line').classList.remove('terminal-line-active');
-    document.getElementsByTagName('body')[0].classList.remove('loading');
-  }, 70)
-}
-
+/*
+ * (Recursively) simulate the user typing a command in
+ * the terminal by revealing the characters one by
+ * one with a slight delay.
+ */
 function revealCharacters(el, chars, callback) {
-  if (!chars.length && callback) return callback();
+  if (!el || !chars || !callback) throw new Error('Missing a required argument for revealCharacters');
+  if (!chars.length) return callback();
 
-  const char = chars[0];
-  el.textContent += char;
+  el.textContent += chars[0];
 
   return setTimeout(function() {
     revealCharacters(el, chars.slice(1), callback);
   }, 70)
 }
 
-loadingCommandEl.classList.remove('hidden');
+/*
+ * When all characters have been revealed, simulate
+ * pressing "Enter" and reveal the rest of the page.
+ */
+function onComplete() {
+  return setTimeout(function() {
+    document.getElementById('terminal-loading-line').classList.remove('terminal-line-active');
+    document.getElementsByTagName('body')[0].classList.remove('loading');
+  }, 70)
+}
 
-revealCharacters(loadingCommandEl, chars, onLoaded);
+window.onload = function() {
+  const loadingLineEl = document.getElementById('terminal-loading-line');
+  const loadingCommandEl = document.getElementById('terminal-loading-command');
+  const loadingCommandText = loadingCommandEl.textContent;
+  const chars = loadingCommandText.split('');
+
+  loadingCommandEl.textContent = '';
+  loadingCommandEl.classList.remove('hidden');
+
+  loadingLineEl.classList.add('terminal-line-active')
+
+  revealCharacters(loadingCommandEl, chars, onComplete);
+};
